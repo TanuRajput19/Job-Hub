5236a5e4cd6d9bc0c49a968f9edef875ed9fedac
Tanu Rajput <rajput.tanu200331@gmail.com>
2025-08-10 13:59:44 +0530

    resolve 401 problem

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cors({
     origin: "https://job-hub-frontend.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization", "Set-Cookie"]
 }));
 
 // ✅ Preflight request handle
